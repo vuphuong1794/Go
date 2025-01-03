cb07e22b19666e37f699ae39eca044805f2de61f
vuphuong1794 <phuongvu1794@gmail.com>
2025-01-03 11:52:12 +0700

    get user input

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
-package main;
+package main
 
-import "fmt";
+import "fmt"
 
-func main(){
-	fmt.Println("hello go");
+func main() {
+	fmt.Println("hello go")
+
+	var name string
+	var age int
+
+	fmt.Scan(&name, &age)
+	fmt.Printf("ten %v tuoi %v", name, age)
 }
