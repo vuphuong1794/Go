44b292e52ae5ecf1c0bb7467fd8c1d85ad25428c
vuphuong1794 <phuongvu1794@gmail.com>
2025-01-06 10:33:58 +0700

    array

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("hello go")
 
-	var name string
-	var age int
+	// var name string
+	// var age int
+	var bookings [50]string
 
-	fmt.Scan(&name, &age)
-	fmt.Printf("ten %v tuoi %v", name, age)
+	bookings[0] = "booking 1"
+
+	fmt.Printf("The whole array: %v \n", bookings)
+	fmt.Printf("The first element: %v \n", bookings[0])
+	fmt.Printf("Array type: %T \n", bookings)
+	fmt.Printf("Array length: %v \n", len(bookings))
+
+	//fmt.Scan(&name, &age)
+	//fmt.Printf("ten %v tuoi %v", name, age)
 }
