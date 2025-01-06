e338f404265d521f59e5db68e6a61b532048d404
vuphuong1794 <phuongvu1794@gmail.com>
2025-01-06 10:42:38 +0700

    slices

--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import "fmt"
 func main() {
 	fmt.Println("hello go")
 
-	// var name string
-	// var age int
-	var bookings [50]string
+	var firstName string
+	var lastName string
+	var bookings [50]string //array
+	var bookings2 []string  //slice
+
+	fmt.Println("Enter your name and last name: ")
+	fmt.Scan(&firstName, &lastName)
 
 	bookings[0] = "booking 1"
+
+	bookings2 = append(bookings2, firstName+" "+lastName)
 
 	fmt.Printf("The whole array: %v \n", bookings)
 	fmt.Printf("The first element: %v \n", bookings[0])
 	fmt.Printf("Array type: %T \n", bookings)
-	fmt.Printf("Array length: %v \n", len(bookings))
+	fmt.Printf("Array length: %v \n\n", len(bookings))
 
-	//fmt.Scan(&name, &age)
-	//fmt.Printf("ten %v tuoi %v", name, age)
+	fmt.Printf("All bookings in slice: %v \n", bookings2)
+	//fmt.Printf("ten %v tuoi %v", firstName, lastName)
 }
