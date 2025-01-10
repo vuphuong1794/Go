e3f76d30e632921d40382654d2d4213a5271e8a9
vuphuong1794 <phuongvu1794@gmail.com>
2025-01-10 14:12:37 +0700

    func

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"strings"
 )
 
+func checkIsValid(x string) bool {
+	if len(x) < 5 {
+		return false
+	}
+	return true
+}
+
 func main() {
 	var firstName string
 	var lastName string
+	var email string
 	conferenceName := "GO Conference"
 	const conferenceTickets = 50
 	var remaingTickets uint = 50
@@ -22,12 +30,23 @@ func main() {
 
 	//for i := 0; i < 50; i++ {}
 
-	for {
+	for { //infinite loop
 		fmt.Println("Enter your firstName and last name: ")
 		fmt.Scan(&firstName, &lastName)
 
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+
 		fmt.Println("Enter number of tickets you want to book: ")
 		fmt.Scan(&userTickets)
+
+		if lengthOfName := len(firstName) + len(lastName); lengthOfName < 3 {
+			fmt.Println("Your first name and last name must be at least 3 characters long")
+			continue
+		}
+		if lengthOfEmail := len(email); lengthOfEmail < 1 {
+			fmt.Println("Email is invalid")
+		}
 
 		if userTickets > remaingTickets {
 			fmt.Printf("Sorry %v %v, we only have %v tickets remaining \n", firstName, lastName, remaingTickets)
@@ -46,7 +65,14 @@ func main() {
 			var names = strings.Fields(booking)
 			firstNames = append(firstNames, names[0])
 		}
+
 		fmt.Printf("these are all our bookings: %v \n", bookings)
 		fmt.Printf("these are all our first names: %v  \n", firstNames)
+
+		if userTickets == 0 {
+			//end program
+			fmt.Println("Our conference is full now. Thank you for booking tickets")
+			break
+		}
 	}
 }
